af473e9a91d956c14cafd086b6cfc59acdb801e3
Kristin Jackvony <kristin.jackvony@nextsteplivinginc.com>
2019-02-08 17:41:08 -0500

    First commit

---
+++ test.js
@@ -0,0 +1,26 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+driver.get('http://www.google.com');
+
+driver.findElement(By.name('q')).sendKeys('webdriver\n');
+
+driver.findElement(By.partialLinkText("seleniumhq")).click();
+
+driver.wait(until.elementLocated(By.id('sidebar')));
+  
+driver.getTitle().then(function(title) {
+    if(title === 'Selenium WebDriver') {
+      console.log('Test passed');
+    } else {
+      console.log('Test failed');
+    }
+    driver.quit();
+});
+
+
